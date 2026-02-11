663d1d7940b1dd17d90baf30ca767d9c7435cbcc
Eduardo Russo <russoedu@gmail.com>
2026-02-11 07:34:03 +0000

    added notifier

--- src/_controllers/message.ts
+++ src/_controllers/message.ts
@@ -1,10 +1,15 @@
 import { MessageUpsertI } from '../_types/types'
+import notifier from 'node-notifier'
+
 class Message {
   upsertHandler ({ type, messages }: MessageUpsertI) {
     if (type === 'notify') { // new messages
       for (const message of messages) {
         console.log(message)
-      // messages is an array, do not just handle the first message, you will miss messages
+        notifier.notify({
+          title:   message.pushName!,
+          message: message.message?.conversation ?? 'No message content',
+        })
       }
     } else { // old already seen / handled messages
     // handle them however you want to
--- src/_models/Message.ts
+++ src/_models/Message.ts
@@ -1,3 +1,4 @@
 export class Message {
-  
+  user: string
+  message: string
 }
